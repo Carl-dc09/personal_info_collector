4a1e4accaebfec910e7193bcee154b5c8019101f
unknown <diamantecarl9@gmail.com>
2024-11-29 17:31:31 +0800

    some addition

--- prog_one_info_collect.py
+++ prog_one_info_collect.py
@@ -5,16 +5,36 @@ def collect_info():
     while True:
 
         # Ask for the informations
-        full_name = input("Full name: ")
-        user_age = input("Age: ")
-        loc_address = input("Address: ")
-        personal_email = input("Email: ")
-        phone_number = input("Phone Number: ")
+        full_name = input("Full name: ").strip()
 
+        while True:
+            try:
+                user_age = int(input("Age: "))
+                break
+        
+            except ValueError:
+                print("Invalid input. Please try again.")
+
+        loc_address = input("Address: ").strip()
+        personal_email = input("Email: ").strip()
+
+        while True:
+            try:
+                phone_number = input("Phone Number: ")
+                if len(phone_number) != 11:
+                    raise ValueError("Phone number must only consist a 11-digit number.")
+                phone_number = int(phone_number)
+                break
+            
+            except ValueError as error:
+                print(error, "Input invalid. Please input 11-digit numbers only.")
+                
+        # Ask for another user input
         another = input("Do you want to input another person's information? (Yes/No): ")
 
         if another != "Yes":
-            break 
+            print("\nInformation Saved Successfully!")
+            break
 
 if __name__ == "__main__":
     collect_info()
