6127c38466d60c708fa1d5d3335874bd0aeb2395
unknown <diamantecarl9@gmail.com>
2024-11-29 18:15:19 +0800

    open the txt file

--- prog_two_info_search.py
+++ prog_two_info_search.py
@@ -1,1 +1,13 @@
 # Second Program
+
+# 
+def open_info():
+    try:
+        with open("personal_info.txt", "r") as file:
+            data = file.read()
+    except FileNotFoundError:
+        print("Error: 'personal_info.txt' not found. Please run Program 1 first to create a file.")
+        return
+    
+if __name__ == "__main__":
+    open_info()
