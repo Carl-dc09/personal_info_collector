c59bd336933382216fb056cf63f00b04bb97ed58
unknown <diamantecarl9@gmail.com>
2024-11-29 18:34:14 +0800

    working name input

--- prog_two_info_search.py
+++ prog_two_info_search.py
@@ -1,13 +1,16 @@
 # Second Program
 
-# 
+# Open the txt file and serach the information that is being asked
 def open_info():
     try:
         with open("personal_info.txt", "r") as file:
-            data = file.read()
+            data_info = file.read()
     except FileNotFoundError:
         print("Error: 'personal_info.txt' not found. Please run Program 1 first to create a file.")
         return
+    
+    while True:
+        search_name = input("Enter the full name: ")
     
 if __name__ == "__main__":
     open_info()
