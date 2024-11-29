f437b413b9ecd4d07ea1b9206a95d2153745ec4d
unknown <diamantecarl9@gmail.com>
2024-11-29 16:44:25 +0800

    Working inputs

---
+++ prog_one_info_collect.py
@@ -0,0 +1,18 @@
+# First program 
+
+# Collect the informations
+def collect_info():
+    while True:
+
+        # Ask for the informations
+        full_name = input("Full name: ")
+        user_age = input("Age: ")
+        loc_address = input("Address: ")
+        personal_email = input("Email: ")
+        phone_number = input("Phone Number: ")
+
+if __name__ == "__main__":
+    collect_info()
+
+         
+
